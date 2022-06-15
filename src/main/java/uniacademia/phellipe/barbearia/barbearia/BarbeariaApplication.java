e2061c4bbab69e4f875d12a8d71835f1e685ae12
phellipeduarte <phellipe.duarte@icloud.com>
2022-06-15 15:09:55 -0300

    primeiro commit

---
+++ src/main/java/uniacademia/phellipe/barbearia/barbearia/BarbeariaApplication.java
@@ -0,0 +1,13 @@
+package uniacademia.phellipe.barbearia.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaApplication.class, args);
+	}
+
+}
