2d93a1e15693ca093bfeaf6223dae35ffa0ad953
phellipeduarte <phellipe.duarte@icloud.com>
2022-06-15 16:54:33 -0300

    configurando swagger e application properties

---
+++ src/main/java/uniacademia/phellipe/barbearia/barbearia/resource/ClienteResource.java
@@ -0,0 +1,13 @@
+package uniacademia.phellipe.barbearia.barbearia.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteResource {
+
+    @GetMapping
+    public String test(){ return "funcionando"; }
+}
