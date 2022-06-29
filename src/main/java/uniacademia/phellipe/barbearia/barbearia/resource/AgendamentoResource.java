cea9b6aebed32dd5075703166a2d83de28eae24c
phellipeduarte <phellipe.duarte@icloud.com>
2022-06-28 22:55:26 -0300

    adicionando permissionamento às rotas necessárias

--- src/main/java/uniacademia/phellipe/barbearia/barbearia/resource/AgendamentoResource.java
+++ src/main/java/uniacademia/phellipe/barbearia/barbearia/resource/AgendamentoResource.java
@@ -1,23 +1,21 @@
 package uniacademia.phellipe.barbearia.barbearia.resource;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uniacademia.phellipe.barbearia.barbearia.DAO.UsuarioDAO;
 import uniacademia.phellipe.barbearia.barbearia.DTO.AgendamentoDTO;
 import uniacademia.phellipe.barbearia.barbearia.model.Agendamento;
 import uniacademia.phellipe.barbearia.barbearia.service.AgendamentoService;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/agendamento")
+@PreAuthorize("isAuthenticated()")
 public class AgendamentoResource {
 
     @Autowired
@@ -26,6 +24,7 @@ public class AgendamentoResource {
     AgendamentoService agendamentoService;
 
     @GetMapping("/listar-todos")
+    @PreAuthorize("hasHole('ADMIN')")
     public ResponseEntity<List<Agendamento>> listarAgendamentos(){
         return ResponseEntity.ok(agendamentoService.listarAgendamentos());
     }
@@ -36,31 +35,37 @@ public class AgendamentoResource {
     }
 
     @GetMapping("/listar/dia")
+    @PreAuthorize("hasHole('ADMIN')")
     public ResponseEntity<List<Agendamento>> listarAgendamentosNoDia(){
         return ResponseEntity.ok(agendamentoService.listarAgendamentosNoDia());
     }
 
     @GetMapping("/contar/dia")
+    @PreAuthorize("hasHole('ADMIN')")
     public ResponseEntity<Integer> contarAgendamentosNoDia() {
         return ResponseEntity.ok(agendamentoService.contarAgendamentosNoDia());
     }
 
     @GetMapping("/listar/semana")
+    @PreAuthorize("hasHole('ADMIN')")
     public ResponseEntity<List<Agendamento>> listarAgendamentosNaSemana(){
         return ResponseEntity.ok(agendamentoService.listarAgendamentosNaSemana());
     }
 
     @GetMapping("/contar/semana")
+    @PreAuthorize("hasHole('ADMIN')")
     public ResponseEntity<Integer> contarAgendamentosNaSemana(){
         return ResponseEntity.ok(agendamentoService.contarAgendamentosNaSemana());
     }
 
     @GetMapping("/listar/mes")
+    @PreAuthorize("hasHole('ADMIN')")
     public ResponseEntity<List<Agendamento>> listarAgendamentosNoMes(){
         return ResponseEntity.ok(agendamentoService.listarAgendamentosNoMes());
     }
 
     @GetMapping("/contar/mes")
+    @PreAuthorize("hasHole('ADMIN')")
     public ResponseEntity<Integer> contarAgendamentosNoMes(){
         return ResponseEntity.ok(agendamentoService.contarAgendamentosNoMes());
     }
--- src/main/java/uniacademia/phellipe/barbearia/barbearia/resource/EquipeResource.java
+++ src/main/java/uniacademia/phellipe/barbearia/barbearia/resource/EquipeResource.java
@@ -2,6 +2,7 @@ package uniacademia.phellipe.barbearia.barbearia.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uniacademia.phellipe.barbearia.barbearia.DTO.CadastroEquipeDTO;
 import uniacademia.phellipe.barbearia.barbearia.model.Equipe;
@@ -17,11 +18,13 @@ public class EquipeResource {
     EquipeService equipeService;
 
     @GetMapping()
+    @PreAuthorize("hasHole('ADMIN')")
     private ResponseEntity<List<Equipe>> listarMembrosEquipe(){
         return ResponseEntity.ok(equipeService.listar());
     }
 
     @PostMapping("/criar")
+    @PreAuthorize("hasHole('ADMIN')")
     private ResponseEntity<Equipe> criarMembroEquipe(@RequestBody CadastroEquipeDTO equipeDTO){
         return ResponseEntity.ok(equipeService.criar(equipeDTO));
     }
